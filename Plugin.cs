166874c6606242b46cdafbd12835b51da5bc022a
stiffmeds <6158160+stiffmeds@users.noreply.github.com>
2023-11-15 19:46:42 +0800

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,165 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Steamworks.Data;
+using Steamworks;
+using System;
+using UnityEngine;
+
+namespace SkipSteam
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("AcrossTheObelisk.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+        internal static ManualLogSource Log;
+        private void Awake()
+        {
+            Log = Logger;
+            harmony.PatchAll();
+            LogInfo($"{PluginInfo.PLUGIN_GUID} {PluginInfo.PLUGIN_VERSION} has loaded!");
+        }
+        internal static void LogDebug(string msg)
+        {
+            Log.LogDebug(msg);
+        }
+        internal static void LogInfo(string msg)
+        {
+            Log.LogInfo(msg);
+        }
+        internal static void LogWarning(string msg)
+        {
+            Log.LogWarning(msg);
+        }
+        internal static void LogError(string msg)
+        {
+            Log.LogError(msg);
+        }
+    }
+
+    internal class SupportingActs
+    {
+        public static void OnGameLobbyJoinRequested(Lobby _lobby, SteamId _friendId)
+        {
+            Debug.Log((object)nameof(OnGameLobbyJoinRequested));
+            Debug.Log((object)_lobby.Id);
+            Debug.Log((object)_friendId);
+            SteamMatchmaking.JoinLobbyAsync(_lobby.Id);
+        }
+
+        public static void OnNewLaunchParameters()
+        {
+            Debug.Log((object)nameof(OnNewLaunchParameters));
+            Debug.Log((object)("[Steam] launchParam -> " + SteamApps.GetLaunchParam("+connect_lobby")));
+        }
+
+        public static void OnChatMessage(Friend _friendId, string _string0, string _string1)
+        {
+            Debug.Log((object)nameof(OnChatMessage));
+            Debug.Log((object)_friendId);
+            Debug.Log((object)_string0);
+            Debug.Log((object)_string1);
+        }
+
+        public static void OnGameRichPresenceJoinRequested(Friend _friendId, string _action)
+        {
+            Debug.Log((object)nameof(OnGameRichPresenceJoinRequested));
+            Debug.Log((object)_friendId);
+            Debug.Log((object)_action);
+        }
+
+        public static void OnLobbyCreated(Result result, Lobby _lobby)
+        {
+            SteamManager.Instance.lobby = _lobby;
+            SteamManager.Instance.lobby.SetPublic();
+            SteamManager.Instance.lobby.SetJoinable(true);
+            SteamManager.Instance.lobby.SetData("RoomName", NetworkManager.Instance.GetRoomName());
+            Debug.Log((object)("[Lobby] OnLobbyCreated " + SteamManager.Instance.lobby.Id.ToString()));
+            SteamFriends.OpenGameInviteOverlay(SteamManager.Instance.lobby.Id);
+        }
+
+        public static void OnLobbyMemberJoined(Lobby _lobby, Friend _friendId)
+        {
+        }
+
+        public static void OnLobbyEntered(Lobby _lobby)
+        {
+            Debug.Log((object)"[Lobby] OnLobbyEntered");
+            if (_lobby.IsOwnedBy(SteamManager.Instance.steamId))
+                return;
+            string data = _lobby.GetData("RoomName");
+            Debug.Log((object)("Steam wants to join room -> " + data));
+            NetworkManager.Instance.WantToJoinRoomName = data;
+            SteamManager.Instance.steamLoaded = true;
+        }
+
+        public static void OnLobbyInvite(Friend _friendId, Lobby _lobby)
+        {
+            Debug.Log((object)"[Lobby] OnLobbyInvite");
+            Debug.Log((object)_friendId);
+        }
+    }
+
+    [HarmonyPatch]
+    public class Skipper
+    {
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SteamManager), "DoSteam")]
+        public static bool DoSteamPrefix(ref SteamManager __instance)
+        {
+            Plugin.Log.LogDebug("STEAM SKIPPER SKIPPING!");
+            uint releaseAppId = 1385380;
+            try
+            {
+                SteamClient.Init(releaseAppId);
+            }
+            catch (System.Exception)
+            {
+                __instance.steamConnected = false;
+            }
+            if (!__instance.steamConnected)
+                return false;
+            if (SteamApps.IsSubscribedToApp((AppId)releaseAppId))
+                GameManager.Instance.SetDemo(false);
+            __instance.steamName = SteamClient.Name;
+            __instance.steamId = SteamClient.SteamId;
+            __instance.GetDLCInformation();
+            SteamFriends.OnGameRichPresenceJoinRequested += new Action<Friend, string>(SupportingActs.OnGameRichPresenceJoinRequested);
+            SteamMatchmaking.OnLobbyCreated += new Action<Result, Lobby>(SupportingActs.OnLobbyCreated);
+            SteamMatchmaking.OnLobbyMemberJoined += new Action<Lobby, Friend>(SupportingActs.OnLobbyMemberJoined);
+            SteamFriends.OnGameLobbyJoinRequested += new Action<Lobby, SteamId>(SupportingActs.OnGameLobbyJoinRequested);
+            SteamMatchmaking.OnLobbyEntered += new Action<Lobby>(SupportingActs.OnLobbyEntered);
+            SteamMatchmaking.OnLobbyInvite += new Action<Friend, Lobby>(SupportingActs.OnLobbyInvite);
+            SteamFriends.OnChatMessage += new Action<Friend, string, string>(SupportingActs.OnChatMessage);
+            SteamApps.OnNewLaunchParameters += new Action(SupportingActs.OnNewLaunchParameters);
+            SteamApps.GetLaunchParam("+connect_lobby");
+            int num = -1;
+            string s = "";
+            string[] commandLineArgs = Environment.GetCommandLineArgs();
+            for (int index = 0; index < commandLineArgs.Length; ++index)
+            {
+                if (index == num)
+                    s = commandLineArgs[index];
+                else if (commandLineArgs[index] == "+connect_lobby")
+                    num = index + index;
+            }
+            if (s != "")
+            {
+                SteamId lobbyId = (SteamId)ulong.Parse(s);
+                try
+                {
+                    SteamMatchmaking.JoinLobbyAsync(lobbyId);
+                }
+                catch
+                {
+                    __instance.steamLoaded = false;
+                }
+            }
+            else
+                __instance.steamLoaded = true;
+            return false;
+        }
+    }
+}
